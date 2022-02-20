e8d86e03f86fd34a4ebd38665973995648102327
JOY <joybosu@gmail.com>
2022-02-20 23:13:50 +0530

    Welcome to Employee Wage

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace EmployeeWageProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Wage problome :");
+
+            //EmployeeCheck.EmployeePresentOrNot();
+
+            //CalculateEmployeeWage.CalculateWage();
+            //EmployeeSwitchCase.CheckEmployUsingSwitchCase();
+
+            //EmployeeWage.CalEmpWage();
+
+            //MonthlyEmployeeWage.MonthlyWage();
+            EmpHourPerMonth.EmpPerMonth();
+
+        }
+    }
+}
